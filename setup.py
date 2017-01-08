b5beffb099d01aa962f04782b6b407a7e3221f8d
Szymon Lipiński <mabewlun@gmail.com>
2017-01-08 08:17:40 +0100

    Update setup.py classifiers

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
-version = '0.6.0'
+version = '1.0.0'
 
 
 class PyTest(TestCommand):
@@ -34,17 +34,16 @@ setup(
     tests_require=['pytest', 'tox', 'psycopg2'],
     cmdclass={'test': PyTest},
     classifiers=[
-            'Development Status :: 4 - Beta',
+            'Development Status :: 5 - Production/Stable',
             'Intended Audience :: Developers',
             'Natural Language :: English',
             'License :: OSI Approved :: MIT License',
             'Programming Language :: Python',
-            'Programming Language :: Python :: 2.6',
             'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.3',
-            'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
             'Topic :: Database',
+            'Topic :: Database :: Front-Ends',
+            'Topic :: Software Development :: Libraries',
     ],
 )
